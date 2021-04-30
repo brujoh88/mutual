fbe880f33e2519a963b9ebfe94ff11504f19305c
Gustavo Tiseira <gustavotiseira@gmail.com>
2021-04-30 19:22:59 -0300

    configurando puerto y conexion a DB

--- server/server.js
+++ server/server.js
@@ -1,13 +1,40 @@
+require('./config/config')
 const express = require('express')
+const mongoose = require('mongoose')
+
+//=================================================
+/* Para que no aparesca esto
+!(node:10712) DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead.
+!(Use `node --trace-deprecation ...` to show where the warning was created)
+*/
+mongoose.set('useCreateIndex', true)
+
+//! (node:15695) DeprecationWarning: Mongoose: `findOneAndUpdate()` and `findOneAndDelete()` without the `useFindAndModify` option set to false are deprecated. See: https://mongoosejs.com/docs/deprecations.html#findandmodify
+mongoose.set('useFindAndModify', false)
+
+//=================================================
+
 const app = express()
 const route = require('./network/routers')
-const port = 3000
+const port = process.env.PORT
 const cors = require('cors')
 
 app.use(express.static(__dirname + '/dist'))
 app.use(cors())
 
 route(app)
+
+mongoose.connect(
+  process.env.URLDB,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, res) => {
+    if (err) throw err
+    console.log('Base de datos online')
+  }
+)
 
 app.listen(port, () =>
   console.log(`La app esta escuando en la URL http://localhost:${port}`)
