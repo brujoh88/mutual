2376a8427950fe80e2a935c708f072a617e6292a
Gustavo Tiseira <gustavotiseira@gmail.com>
2021-05-03 19:47:35 -0300

    Ruta proovedor

--- server/network/routes.js
+++ server/network/routes.js
@@ -2,11 +2,13 @@
 const componenteModelo = require('../components/componenteModelo/network')
 const otroComponente = require('../components/otroComponente/network')
 const afiliado = require('../components/afiliado/network')
+const proovedor = require('../components/proovedor/network')
 
 const routes = function(server) {
   server.use('/componenteModelo', componenteModelo)
   server.use('/otroComponente', otroComponente)
   server.use('/afiliado', afiliado)
+  server.use('/proovedor', proovedor)
 }
 
 module.exports = routes
