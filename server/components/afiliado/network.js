77564edcdf9a34b78584179dbf84665b4225f1da
Gustavo Tiseira <gustavotiseira@gmail.com>
2021-05-03 17:56:23 -0300

    creacion de afiliado en componente afiliado

--- server/components/afiliado/network.js
+++ server/components/afiliado/network.js
@@ -3,12 +3,37 @@ const router = express.Router()
 const response = require('../../network/response')
 const controller = require('./controller')
 
+/* 
+========================================
+                Leer Afiliado
+========================================
+*/
+
 router.get('/', function(req, res) {
   let legajo = req.query.legajo
   controller
     .getAfiliado(legajo)
     .then((user) => {
       response.success(req, res, user, 200)
+    })
+    .catch((err) => {
+      response.error(req, res, 'Unexpected error', 500, err)
+    })
+})
+
+/* 
+========================================
+                Crear afiliado
+========================================
+*/
+
+router.post('/', function(req, res) {
+  let body = req.body
+  console.log(body)
+  controller
+    .postAfiliado(body)
+    .then((resp) => {
+      response.success(req, res, resp, 201)
     })
     .catch((err) => {
       response.error(req, res, 'Unexpected error', 500, err)
