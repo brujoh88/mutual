e0a6487c07f8334dfb67a091771d014f357226ea
Gustavo Tiseira <gustavotiseira@gmail.com>
2021-05-05 17:07:04 -0300

    eliminacion del componente otroComponente + moficacion en routes.js

--- server/network/routes.js
+++ server/network/routes.js
@@ -1,12 +1,10 @@
 //const express = require('express')
 const componenteModelo = require('../components/componenteModelo/network')
-const otroComponente = require('../components/otroComponente/network')
 const afiliado = require('../components/afiliado/network')
 const proovedor = require('../components/proovedor/network')
 
 const routes = function(server) {
   server.use('/componenteModelo', componenteModelo)
-  server.use('/otroComponente', otroComponente)
   server.use('/afiliado', afiliado)
   server.use('/proovedor', proovedor)
 }
