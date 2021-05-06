ed647a56b00587c3f2d1bf17a1aaba7afa868d1e
Gustavo Tiseira <gustavotiseira@gmail.com>
2021-05-06 15:02:47 -0300

    ruteo a componente cuota

--- server/network/routes.js
+++ server/network/routes.js
@@ -2,11 +2,13 @@
 const orden = require('../components/orden/network')
 const afiliado = require('../components/afiliado/network')
 const proovedor = require('../components/proovedor/network')
+const cuota = require('../components/cuota/network')
 
 const routes = function(server) {
   server.use('/orden', orden)
   server.use('/afiliado', afiliado)
   server.use('/proovedor', proovedor)
+  server.use('/cuota', cuota)
 }
 
 module.exports = routes
