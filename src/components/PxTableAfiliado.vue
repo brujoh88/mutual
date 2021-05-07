2d82113990b80c124601d0245136701f20ed970e
Gustavo Tiseira <gustavotiseira@gmail.com>
2021-05-07 02:33:12 -0300

    haciendo pruebas con bootstrap-Vue

--- src/components/PxTableAfiliado.vue
+++ src/components/PxTableAfiliado.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
     <p>HOLAAA FUNCIONA Y puedo decir desde el padre {{ msg }}</p>
+    <b-table
+      striped
+      hover
+      :items="items"
+      :fields="fields"
+      :tbody-tr-class="rowClass"
+      :busy="isBusy"
+    >
+      <template #table-busy>
+        <div class="text-center text-danger my-2">
+          <b-spinner class="align-middle"></b-spinner>
+          <strong>Loading...</strong>
+        </div>
+      </template>
+    </b-table>
   </div>
 </template>
 
@@ -8,7 +23,20 @@
 export default {
   name: 'TableAfiliado',
   data() {
-    return { items: null }
+    return {
+      fields: [
+        {
+          key: 'legajo',
+          sortable: true,
+        },
+        { key: 'nombre', sortable: true },
+        { key: 'apellido', sortable: true },
+        { key: 'dni', sortable: true },
+        { key: 'saldoAsignado', sortable: true, label: '$' },
+      ],
+      items: null,
+      isBusy: true,
+    }
   },
   props: {
     msg: String,
@@ -18,11 +46,24 @@ export default {
       .then((response) => {
         return response.json()
       })
-      .then((datos) => (this.items = datos.body))
+      .then((datos) => {
+        this.items = datos.body
+      })
       .catch((error) => {
         console.log(error)
       })
-    /* .finally(() => this.loading = false) */
+      .finally(() => (this.isBusy = false))
+  },
+  methods: {
+    rowClass(item, type) {
+      if (!item || type !== 'row') return
+      if (item.saldoAsignado > 10_000) {
+        console.log('holaaa')
+        return 'table-success'
+      } else {
+        return 'table-warning'
+      }
+    },
   },
 }
 </script>
