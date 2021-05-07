a5318560de7514bfdcc213b7bfd244507b04788a
Gustavo Tiseira <gustavotiseira@gmail.com>
2021-05-07 01:16:41 -0300

    Frontend: Eliminado HelloWorld.vue - Home.vue agregando componente nuevo PxTableAfiliado.vue

---
+++ src/components/PxTableAfiliados.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>HOLAAA FUNCIONA Y puedo decir desde el padre {{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TableAfiliado',
+  data() {
+    return { items: null }
+  },
+  props: {
+    msg: String,
+  },
+  mounted() {
+    fetch('http://localhost:3000/afiliado/')
+      .then((response) => {
+        return response.json()
+      })
+      .then((datos) => (this.items = datos.body))
+      .catch((error) => {
+        console.log(error)
+      })
+    /* .finally(() => this.loading = false) */
+  },
+}
+</script>
+
+<style></style>
