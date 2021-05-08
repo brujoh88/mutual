2e27d9f744d65b0e6f222a5361d3b09b962248bb
Gustavo Tiseira <gustavotiseira@gmail.com>
2021-05-07 21:06:27 -0300

    capturacion del id en vista afiliado

--- src/views/Afiliado.vue
+++ src/views/Afiliado.vue
@@ -3,3 +3,19 @@
     <h1>This is an Afiliado page</h1>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  watch: {
+    '$route.query.id': {
+      immediate: true,
+      handler(id) {
+        console.log(id)
+      },
+    },
+  },
+}
+</script>
