acf5d347696e3ac44fd459193c00a8fcfa23c0ca
Gustavo Tiseira <gustavotiseira@gmail.com>
2021-05-08 19:08:19 -0300

    creacion parcial del formulario en el Modal

--- src/components/PxModalNewAfiliado.vue
+++ src/components/PxModalNewAfiliado.vue
@@ -6,64 +6,79 @@
     >
 
     <!-- The modal -->
-    <b-modal id="my-modal" centered title="Nuevo Afiliado"
+    <b-modal id="my-modal" centered title="Nuevo Afiliado" v-model="show"
       ><div>
-        <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-          <b-form-group
-            id="input-group-1"
-            label="Email address:"
-            label-for="input-1"
-            description="We'll never share your email with anyone else."
-          >
+        <b-form @submit="onSubmit" @reset="onReset" v-if="showForm">
+          <b-form-group id="input-group-0" label="Legajo:" label-for="input-0">
+            <b-form-input
+              id="input-0"
+              v-model="form.legajo"
+              placeholder="Ingrese el legajo aqui"
+              required
+            ></b-form-input>
+          </b-form-group>
+          <b-form-group id="input-group-1" label="Nombre:" label-for="input-1">
             <b-form-input
               id="input-1"
-              v-model="form.email"
-              type="email"
-              placeholder="Enter email"
+              v-model="form.nombre"
+              placeholder="Ingrese el nombre aqui"
               required
             ></b-form-input>
           </b-form-group>
 
           <b-form-group
             id="input-group-2"
-            label="Your Name:"
+            label="Apellido:"
             label-for="input-2"
           >
             <b-form-input
               id="input-2"
-              v-model="form.name"
-              placeholder="Enter name"
+              v-model="form.apellido"
+              placeholder="Ingrese el apellido aqui"
               required
             ></b-form-input>
           </b-form-group>
 
-          <b-form-group id="input-group-3" label="Food:" label-for="input-3">
-            <b-form-select
+          <b-form-group id="input-group-3" label="DNI:" label-for="input-3">
+            <b-form-input
               id="input-3"
-              v-model="form.food"
-              :options="foods"
+              v-model="form.dni"
+              placeholder="Ingrese el DNI aqui"
               required
-            ></b-form-select>
+            ></b-form-input>
           </b-form-group>
 
-          <b-form-group id="input-group-4" v-slot="{ ariaDescribedby }">
-            <b-form-checkbox-group
-              v-model="form.checked"
-              id="checkboxes-4"
-              :aria-describedby="ariaDescribedby"
-            >
-              <b-form-checkbox value="me">Check me out</b-form-checkbox>
-              <b-form-checkbox value="that">Check that out</b-form-checkbox>
-            </b-form-checkbox-group>
+          <b-form-group
+            id="input-group-4"
+            label="Saldo Asignado:"
+            label-for="input-4"
+          >
+            <b-form-input
+              id="input-4"
+              v-model="form.saldoAsignado"
+              placeholder="Ingrese el salgo a asignar"
+              required
+            ></b-form-input>
           </b-form-group>
 
-          <b-button type="submit" variant="primary">Submit</b-button>
-          <b-button type="reset" variant="danger">Reset</b-button>
+          <b-button type="submit" variant="success" class="mr-3"
+            >Guardar</b-button
+          >
+          <b-button type="reset" variant="warning">Resetear</b-button>
         </b-form>
-        <b-card class="mt-3" header="Form Data Result">
-          <pre class="m-0">{{ form }}</pre>
-        </b-card>
-      </div></b-modal
+      </div>
+      <template #modal-footer>
+        <div class="w-100">
+          <b-button
+            variant="danger"
+            size="sm"
+            class="float-right"
+            @click="show = false"
+          >
+            Cerrar
+          </b-button>
+        </div>
+      </template></b-modal
     >
   </div>
 </template>
@@ -73,19 +88,14 @@ export default {
   data() {
     return {
       form: {
-        email: '',
-        name: '',
-        food: null,
-        checked: [],
+        legajo: '',
+        nombre: '',
+        apellido: '',
+        dni: '',
+        saldoAsignado: '',
       },
-      foods: [
-        { text: 'Select One', value: null },
-        'Carrots',
-        'Beans',
-        'Tomatoes',
-        'Corn',
-      ],
-      show: true,
+      showForm: true,
+      show: false,
     }
   },
   methods: {
@@ -96,14 +106,15 @@ export default {
     onReset(event) {
       event.preventDefault()
       // Reset our form values
-      this.form.email = ''
-      this.form.name = ''
-      this.form.food = null
-      this.form.checked = []
+      this.form.legajo = ''
+      this.form.nombre = ''
+      this.form.apellido = ''
+      this.form.dni = ''
+      this.form.saldoAsignado = ''
       // Trick to reset/clear native browser form validation state
-      this.show = false
+      this.showForm = false
       this.$nextTick(() => {
-        this.show = true
+        this.showForm = true
       })
     },
   },
