0c602fc5525e87af7d3d5bf9a52d53e0b11ffc98
Gustavo Tiseira <gustavotiseira@gmail.com>
2021-05-08 17:48:22 -0300

    New componente PxModalNewAfiliado.vue + incorporacion en HOME.vue

---
+++ src/components/PxModalNewAfiliado.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <!-- Using value -->
+    <b-button v-b-modal="'my-modal'" variant="success"
+      >Agreguar Afiliado +</b-button
+    >
+
+    <!-- The modal -->
+    <b-modal id="my-modal" centered title="BootstrapVue"
+      >Hello From My Modal!</b-modal
+    >
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <ModalNewAfiliado />
     <TableAfiliado msg="Soy el padre(Home.vue)" />
   </div>
 </template>
@@ -7,10 +8,13 @@
 <script>
 // @ is an alias to /src
 import TableAfiliado from '@/components/PxTableAfiliado.vue'
+import ModalNewAfiliado from '@/components/PxModalNewAfiliado.vue'
+
 export default {
   name: 'Home',
   components: {
     TableAfiliado,
+    ModalNewAfiliado,
   },
 }
 </script>
