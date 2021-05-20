5c994383e2b3d305fb5bb5f8ec97e8f72ab75353
Gustavo Tiseira <gustavotiseira@gmail.com>
2021-05-20 20:52:28 -0300

    Trabajando logica de calculo de apertura y cierre de periodo - primeras implementaciones desde el frontend

--- server/components/calculoPeriodo/network.js
+++ server/components/calculoPeriodo/network.js
@@ -4,10 +4,63 @@ const response = require('../../network/response')
 const controller = require('./controller')
 
 router.get('/:fecha', function(req, res) {
-  let fechaActual = req.params.fecha
-  console.log(fechaActual)
+  const DIA_SEMANA = {
+    0: 'Domingo',
+    1: 'Lunes',
+    2: 'Martes',
+    3: 'Miercoles',
+    4: 'Jueves',
+    5: 'Viernes',
+    6: 'Sabado',
+  }
+  const MESES_DEL_ANIO = {
+    0: 'Enero',
+    1: 'Febrero',
+    2: 'Marzo',
+    3: 'Abril',
+    4: 'Mayo',
+    5: 'Junio',
+    6: 'Julio',
+    7: 'Agosto',
+    8: 'Septiembre',
+    9: 'Octubre',
+    10: 'Noviembre',
+    11: 'Diciembre',
+  }
+
+  const MOVIMIENTO_SEGUN_DIA_SEMANA = {
+    Domingo: -2,
+    Lunes: -3,
+    Martes: -4,
+    Miercoles: 2,
+    Jueves: 1,
+    Viernes: 0,
+    Sabado: -1,
+  }
+
+  let fechaActual = new Date(req.params.fecha)
+  let anioActual = fechaActual.getFullYear()
+  let mesActual = fechaActual.getMonth()
+  let diaActual = fechaActual.getDate()
+  let diaSemanaActual = fechaActual.getDay()
+
+  let numeroSemanaDiaDiez = new Date(anioActual, mesActual, 10).getDay()
+
+  let diaSemanaDelDiez = DIA_SEMANA[numeroSemanaDiaDiez]
+
+  let fechaCierreActual = new Date(
+    anioActual,
+    mesActual,
+    10 + MOVIMIENTO_SEGUN_DIA_SEMANA[diaSemanaDelDiez]
+  )
+
   res.json({
-    ok: true,
+    fecha: fechaActual,
+    diaActual,
+    mesActual: MESES_DEL_ANIO[mesActual],
+    diaSemanaActual: DIA_SEMANA[diaSemanaActual],
+    numeroSemanaDiaDiez,
+    fecha_cierre: fechaCierreActual,
   })
 })
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    {{ dia }}
     <ModalNewAfiliado v-on:isPost="forceRender" class="mb-3" />
     <TableAfiliado :key="key1" />
   </div>
@@ -19,12 +20,28 @@ export default {
   data() {
     return {
       key1: 0,
+      dia: '',
     }
   },
   methods: {
     forceRender() {
       this.key1 += 1
     },
+  },
+  mounted() {
+    let fecha = new Date()
+    fetch(`http://localhost:3000/calculoPeriodo/${fecha}`)
+      .then((response) => {
+        return response.json()
+      })
+      .then((datos) => {
+        let fechaCierrePeriodo = new Date(datos.fecha_cierre)
+        this.dia = fechaCierrePeriodo
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+      .finally(() => console.log('genial termino'))
   },
 }
 </script>
