8547153fbc8e439daae12388c5d3f130a7866f0a
Gustavo Tiseira <gustavotiseira@gmail.com>
2021-07-10 09:45:14 -0300

    Quitado de comentarios

--- server/components/calculoPeriodo/controller.js
+++ server/components/calculoPeriodo/controller.js
@@ -84,42 +84,6 @@ const calcularPeriodo = (fechaActual) => {
   })
 }
 
-/* const getProovedors = () => {
-  return new Promise((resolve, reject) => {
-    store
-      .getProovedorsDB()
-      .then((proovedors) => resolve(proovedors))
-      .catch((err) => reject(err))
-  })
-}
-
-const getProovedor = (idProovedor) => {
-  return new Promise((resolve, reject) => {
-    store
-      .getProovedorDB(idProovedor)
-      .then((proovedor) => resolve(proovedor))
-      .catch((err) => reject(err))
-  })
-}
-
-const postProovedor = (body) => {
-  return new Promise((resolve, reject) => {
-    store
-      .postProovedorDB(body)
-      .then((resp) => resolve(resp))
-      .catch((err) => reject(err))
-  })
-}
-
-const putProovedor = (id, datos) => {
-  return new Promise((resolve, reject) => {
-    store
-      .putProovedorDB(id, datos)
-      .then((resp) => resolve(resp))
-      .catch((err) => reject(err))
-  })
-} */
-
 module.exports = {
   calcularPeriodo,
 }
