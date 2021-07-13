0feda4b6395683706f0df3ca1cb0dd1a6b81c19f
Gustavo Tiseira <gustavotiseira@gmail.com>
2021-07-13 11:54:19 -0300

    Frontend: Ruteo a la vista /orden

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Afiliado from '../views/Afiliado.vue'
 import Configuracion from '../views/Configuracion.vue'
+import OrdenById from '../views/orden.vue'
 
 Vue.use(VueRouter)
 
@@ -30,6 +31,11 @@ const routes = [
     path: '/config',
     name: 'configuracion',
     component: Configuracion,
+  },
+  {
+    path: '/orden',
+    name: 'orden',
+    component: OrdenById,
   },
 ]
 
