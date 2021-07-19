150c449c1ac2a7867029c1bd0477cd180398d998
Gustavo Tiseira <gustavotiseira@gmail.com>
2021-07-19 17:03:51 -0300

    Frontend: Incorporando paquete

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueJsonToCsv from 'vue-json-to-csv'
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(VueJsonToCsv)
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
