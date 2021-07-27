1e38a4b0b154bfe84809f86f04c71dd5b497b15b
Gustavo Tiseira <gustavotiseira@gmail.com>
2021-07-26 21:12:01 -0300

    Frontend: Correccion en la validacion

--- src/components/PxModalOrden.vue
+++ src/components/PxModalOrden.vue
@@ -212,9 +212,11 @@ export default {
         (this.form.proovedor != null &&
           this.form.cuota != null &&
           this.saldoDisponible >= this.form.monto &&
-            this.form.monto != 0 &&
-            this.form.monto > 0) ||
-        (this.form.monto / this.form.cuota <= this.saldoDisponible &&
+          this.form.monto != 0 &&
+          this.form.monto > 0) ||
+        (this.form.proovedor != null &&
+          this.form.cuota != null &&
+          this.form.monto / this.form.cuota <= this.saldoDisponible &&
           this.form.monto != 0 &&
           this.form.monto > 0)
       )
