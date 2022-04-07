e17e4bf57cef52b3e62a30886d18947e0f8ac43c
gtiseiraMVC <gustavotiseira@gmail.com>
2022-04-07 08:43:11 -0300

    Fecha custom

--- server/components/calculoPeriodo/helpers.js
+++ server/components/calculoPeriodo/helpers.js
@@ -11,4 +11,12 @@ module.exports = {
     { dia: 9, mes: 0, anio: 2022 },
     { dia: 12, mes: 1, anio: 2022 },
   ],
+  3: [
+    { dia: 13, mes: 2, anio: 2022 },
+    { dia: 10, mes: 3, anio: 2022 },
+  ],
+  4: [
+    { dia: 10, mes: 3, anio: 2022 },
+    { dia: 14, mes: 4, anio: 2022 },
+  ],
 }
