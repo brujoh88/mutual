aeca72a734398c183bdd28121346c5280bba6231
Gus - MVC <gustavotiseira@gmail>
2022-09-01 10:18:18 -0300

    actualizando fecha de cierre

--- server/components/calculoPeriodo/helpers.js
+++ server/components/calculoPeriodo/helpers.js
@@ -11,4 +11,12 @@ module.exports = {
     { dia: 9, mes: 0, anio: 2022 },
     { dia: 12, mes: 1, anio: 2022 },
   ],
+  3: [
+    { dia: 13, mes: 7, anio: 2022 },
+    { dia: 8, mes: 9, anio: 2022 },
+  ],
+  4: [
+    { dia: 8, mes: 9, anio: 2022 },
+    { dia: 15, mes: 9, anio: 2022 },
+  ],
 }
